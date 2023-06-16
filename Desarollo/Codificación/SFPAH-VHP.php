4c41ce7b24c6eb70d4c6e0756451080e9e5d46b3
protoboy <113700916+DincaAlex@users.noreply.github.com>
2023-06-16 12:27:41 -0500

    corregido error donde el grafico no estaba conectado correctamente al database

--- Desarollo/Codificación/SFPAH-VHP.php
+++ Desarollo/Codificación/SFPAH-VHP.php
@@ -1,64 +1,88 @@
 <?php
 session_start();
 
-require_once $_SERVER['DOCUMENT_ROOT'].'\clase.php';
+require_once("clase.php");
 
 $usar_db = new DBControl();
-
 ?>
-    <html>
-    <meta charset="UTF-8">
-    <head>
-        <title>SFPAH</title>
 
-        <link href="css/prodstyle.css" rel="stylesheet" />
-        <link href="css/style.css" rel="stylesheet" />
-
-
-    </head>
+<html>
+<meta charset="UTF-8">
+<head>
+    <title>SFPAH</title>
+    <link href="css/prodstyle.css" rel="stylesheet" />
+    <link href="css/style.css" rel="stylesheet" />
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+</head>
 <body>
-    <div align="center"><h1 style="color: white;">Selección de productos</h1>
-
-
-
-    </div>
-
-    <div align="center">
-
-
-        <form actions="" method="get">
-            <input type="text" name="busqueda"> <br>
-            <input type="submit" name="enviar" value="Buscar">
-
-        </form>
-
-        <br>
-
-    </div>
-
+<div align="center"><h1 style="color: white;">Historial de precios</h1></div>
+<div align="center">
+    <form action="" method="get">
+        <input type="text" name="busqueda" style="width: 300px; height: 30px;" /><br>
+        <input type="submit" name="enviar" value="Buscar" />
+    </form>
+    <br>
+</div>
 <?php
-if(isset($_GET['enviar'])){
-
+if(isset($_GET['enviar']))
+{
     $busqueda = $_GET['busqueda'];
-    $consulta = $usar_db->vaiquery("SELECT * FROM productos WHERE nom LIKE '%$busqueda%'");
-
-
-    foreach($consulta as $i=>$k)
+    $consulta = $usar_db->vaiQuery("SELECT * FROM productos WHERE nom LIKE '%$busqueda%'");
+    foreach($consulta as $i => $k)
     {
         ?>
         <div class="contenedor_productos">
-            <form method="POST" action="prodslct.php?accion=agregar&cod=
-			<?php echo $consulta[$i]["cod"]; ?>">
+            <form method="POST" action="">
                 <div><img src="<?php echo $consulta[$i]["img"]; ?>"></div>
                 <div>
                     <div style="padding-top:20px;font-size:18px;"><?php echo $consulta[$i]["nom"]; ?></div>
                     <div style="padding-top:10px;font-size:20px;"><?php echo "S/.".$consulta[$i]["pre"]; ?></div>
-                    <div><input type="text" name="txtcantidad" value="1" size="2" /><input type="submit" value="Agregar" />
+                    <div>
+                        <input type="hidden" name="cod" value="<?php echo $consulta[$i]["cod"]; ?>" />
+                        <input type="submit" name="agregar" value="Historial" />
                     </div>
                 </div>
             </form>
-        </div></html><?php
-    }
+        </div>
+        <?php
+        if (isset($_POST['agregar'])) {
+            $productCod = $_POST['cod'];
+            $priceData = $usar_db->vaiQuery("SELECT fecha, pre FROM historial_precios WHERE prod_cod = '$productCod'");
 
+            // Prepare data for the graph
+            $dates = array();
+            $prices = array();
+            foreach ($priceData as $data) {
+                $dates[] = $data['fecha'];
+                $prices[] = $data['pre'];
+            }
+
+            // Create the graph using Plotly
+            echo '<div id="graph"></div>';
+            echo "<script>
+                    var dates = " . json_encode($dates) . ";
+                    var prices = " . json_encode($prices) . ";
+                    
+                    var trace = {
+                        x: dates,
+                        y: prices,
+                        type: \"scatter\",
+                        mode: \"lines+markers\",
+                        marker: {size: 6},
+                        line: {shape: \"spline\"}
+                    };
+                    
+                    var layout = {
+                        title: \"Historial de precio\",
+                        xaxis: {title: \"Fecha\"},
+                        yaxis: {title: \"Precio\"}
+                    };
+                    
+                    Plotly.newPlot(\"graph\", [trace], layout);
+                </script>";
+        }
+    }
 }
 ?>
+</body>
+</html>
